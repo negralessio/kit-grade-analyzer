0100967db0994d01bf568c4245c3020acac5e1fc
negralessio <Alessio.Negrini@live.de>
2024-03-23 16:51:11 +0100

    Added feature to add more than 1 URL to analyze

--- gui/app.py
+++ gui/app.py
@@ -5,6 +5,7 @@ import time
 
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -14,78 +15,107 @@ st.set_page_config(layout="wide")
 from src.dataloader import DataLoader
 from src.guard import Guard
 
+import src.utils as utils
+import src.constants as constants
+
 
 def run_gui() -> None:
     _render_sidebar()
     # Get Input URL
-    url: str = st.text_input("Enter URL to PDF to analyze:", placeholder="URL to the cohort PDF ...")
+    input_txt: str = st.text_input("Enter one or more URL to PDF to analyze:", placeholder="URL(s) to the cohort PDF ...")
 
-    if len(url) == 0:
+    if len(input_txt) == 0:
         st.info("Please enter the URL to the PDF document that you'd like to analyze.\n\n" +
                 "You can find the " +
-                "ECTS Ranking Charts here: \n\n  https://www.sle.kit.edu/nachstudium/ects-einstufungstabellen.php",
+                "ECTS Ranking Charts here: \n\n  https://www.sle.kit.edu/nachstudium/ects-einstufungstabellen.php" +
+                f"\n\nNote that you can add two or more URLs to the field, but seperate it using '{constants.SEP}'",
                 icon="ℹ️")
         return
 
+    url_list: list[str] = utils.separate_input_string(input_txt, sep=constants.SEP)
+
     # Check input for validity and safety
-    if not Guard(url).check_input():
-        st.error("Please try a different URL.", icon="⚠️")
-        return
+    for url in url_list:
+        if not Guard(url).check_input():
+            st.error("Please try a different URL.", icon="⚠️")
+            return
 
     # Get Data and perform Analysis
     try:
         if st.button("Analyze 🚀", type="primary"):
             with st.spinner("Crawling and Preprocessing Data ..."):
-                time.sleep(1)
-                dataloader = DataLoader(url)
-                dataloader.load_data()
-                df = dataloader.get_df()
+                df_list = []
+                cohort_list = []
+                for url in url_list:
+                    time.sleep(1)
+                    dataloader = DataLoader(url)
+                    dataloader.load_data()
+                    df = dataloader.get_df()
+                    df_list.append(df)
+                    cohort_list.append(dataloader.get_study())
 
             tab1, tab2, tab3 = st.tabs(
                 ["View Bar Plot", "View Cumulative Distribution (CDF)", "View Raw Data"]
             )
 
             with tab1:
-                _bar_plot_view(df, dataloader)
+                _bar_plot_view(df_list, cohort_list)
             with tab2:
-                _cdf_view(df, dataloader)
+                _cdf_view(df_list, cohort_list)
             with tab3:
-                _raw_data_view(df)
+                _raw_data_view(df_list, cohort_list)
     except IndexError:
         st.error("Please try a different URL.", icon="⚠️")
 
 
-def _bar_plot_view(df, dataloader) -> None:
+def _bar_plot_view(df_list, cohort_list) -> None:
     # View Stats
-    _display_stats(df, dataloader)
+    if len(df_list) == 1:
+        _display_stats(df_list[0], cohort_list[0])
 
-    # Display Bar Plot
-    p = px.bar(df, x="Note", y="Anzahl", title=f"Grade Distribution of Cohort '{dataloader.get_study()}'")
-    p.update_xaxes(tickvals=[i/10 for i in range(10, 41)], ticktext=[str(i/10) for i in range(10, 41)])
-    p.add_vline(x=np.round(np.average(df['Note'], weights=df['Anzahl']), 4),
-                annotation_text="Mean", line_dash="dot", line_color="red")
-    st.plotly_chart(p, use_container_width=True)
-
-
-def _cdf_view(df, dataloader) -> None:
-    # View Stats
-    _display_stats(df, dataloader)
-
-    # Display CDF
-    fig = px.line(df, x="Note", y="Kumuliert", title=f"CDF of Cohort '{dataloader.get_study()}'")
+    fig = go.Figure()
+    for df, cohort in zip(df_list, cohort_list):
+        fig.add_trace(go.Bar(x=df["Note"], y=df["Anzahl"], name=cohort))
     fig.update_xaxes(tickvals=[i / 10 for i in range(10, 41)], ticktext=[str(i / 10) for i in range(10, 41)])
+    fig.update_layout(title='Grade Distribution', xaxis_title='Grade', yaxis_title='Count')
+    fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+    st.plotly_chart(fig, use_container_width=True)
+
+
+def _cdf_view(df_list, cohort_list) -> None:
+
+    # View Stats
+    if len(df_list) == 1:
+        _display_stats(df_list[0], cohort_list[0])
+
+    fig = go.Figure()
+    for df, cohort in zip(df_list, cohort_list):
+        fig.add_trace(go.Line(x=df["Note"], y=df["Kumuliert"], name=cohort))
+    fig.update_xaxes(tickvals=[i / 10 for i in range(10, 41)], ticktext=[str(i / 10) for i in range(10, 41)])
+    fig.update_layout(title='Cumulative Distribution Function (CDF)', xaxis_title='Grade', yaxis_title='Cumulative [%]')
+    fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
     fig.add_hline(y=10, annotation_text="Top 10%", line_dash="dot", line_color="red")
     fig.add_hline(y=50, annotation_text="Median", line_dash="dot", line_color="red")
     st.plotly_chart(fig, use_container_width=True)
 
 
-def _raw_data_view(df) -> None:
-    st.dataframe(df)
+def _raw_data_view(df_list, cohort_list) -> None:
+    N = len(df_list)
+    col1, col2 = st.columns(2)
+
+    for i, (df, cohort) in enumerate(zip(df_list, cohort_list)):
+        if i % 2 == 0:
+            col1.subheader(body=f"{cohort}", divider="blue")
+            col1.dataframe(df)
+        else:
+            col2.subheader(body=f"{cohort}", divider="blue")
+            col2.dataframe(df)
 
 
-def _display_stats(df, dataloader) -> None:
 
-    st.metric(label="Cohort", value=dataloader.get_study())
+def _display_stats(df, cohort) -> None:
+
+    st.metric(label="Cohort", value=cohort)
     col1, col2, col3 = st.columns(3)
     # Display Stats
     mean = np.average(df['Note'], weights=df['Anzahl'])
