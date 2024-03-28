4b090f18b90416c1b2c81852aacf91311db408e8
negralessio <Alessio.Negrini@live.de>
2024-03-28 20:26:07 +0100

    Added utils test

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+# Executed via python -m pytest -v --cov tests/ from root
+import pytest
+import src.utils as utils
+import src.constants as constants
+
+
+@pytest.mark.parametrize(
+    "x, sep, expected",
+    [
+        ("", constants.SEP, [""]),
+        ("URL1", constants.SEP, ["URL1"]),
+        ("URL1$URL2", constants.SEP, ["URL1", "URL2"]),
+        ("URL1$URL2$URL3", constants.SEP, ["URL1", "URL2", "URL3"])
+    ]
+)
+def test_separate_input_string_return_correct_list(x: str, sep: str, expected: list[str]):
+    assert utils.separate_input_string(x, sep) == expected
