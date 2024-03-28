815942a52d2b9e0c7cd52a81cbb4a2bec6be125f
negralessio <Alessio.Negrini@live.de>
2024-03-28 20:25:47 +0100

    Added function doc

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,10 @@
 
 def separate_input_string(x: str, sep: str) -> list[str]:
+    """
+    Separates string based on separator token sep and returns list of strings.
+    E.g. "URL1$URL" --> ["URL1", "URL2"]
+    :param x: str -- Input String
+    :param sep: str -- Separator Token
+    :return: list[str]
+    """
     return x.split(sep)
