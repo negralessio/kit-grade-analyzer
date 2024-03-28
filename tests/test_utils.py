5201ade132cf0244652098d97a9c98f3bda48a51
negralessio <Alessio.Negrini@live.de>
2024-03-28 21:01:10 +0100

    Added more test cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 # Executed via python -m pytest -v --cov tests/ from root
 import pytest
+
 import src.utils as utils
 import src.constants as constants
 
@@ -10,8 +11,14 @@ import src.constants as constants
         ("", constants.SEP, [""]),
         ("URL1", constants.SEP, ["URL1"]),
         ("URL1$URL2", constants.SEP, ["URL1", "URL2"]),
-        ("URL1$URL2$URL3", constants.SEP, ["URL1", "URL2", "URL3"])
+        ("URL1$URL2$URL3", constants.SEP, ["URL1", "URL2", "URL3"]),
+        ("URL1$$$$URL2", constants.SEP, ["URL1", "", "", "", "URL2"])
     ]
 )
 def test_separate_input_string_return_correct_list(x: str, sep: str, expected: list[str]):
     assert utils.separate_input_string(x, sep) == expected
+
+
+def test_separate_input_string_separator_token_empty_return_ValueError():
+    with pytest.raises(ValueError):
+        utils.separate_input_string("URL1$URL2", "")
